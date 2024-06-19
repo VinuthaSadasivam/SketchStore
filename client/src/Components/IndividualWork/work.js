f24caf545b75641c36a914db422dd8d032d928fb
VinuthaSadasivam <125736671+VinuthaSadasivam@users.noreply.github.com>
2024-06-19 13:22:11 +0700

    Update work.js
    changed the src and imageUrl from "http://localhost:1337" to "https://vinuarts.com"

--- client/src/Components/IndividualWork/work.js
+++ client/src/Components/IndividualWork/work.js
@@ -38,7 +38,7 @@ const Item = ({ item, width }) => {
           alt={item.name}
           width="300px"
           height="400px"
-          src={`http://localhost:1337${url}`}
+          src={`https://vinuarts.com${url}`}
           onClick={() => navigate(`/item/${item.id}`)}
           style={{ cursor: "pointer" }}
         />
@@ -80,7 +80,7 @@ const Item = ({ item, width }) => {
                     count: count,
                     size: item.attributes.size,
                     details: details,
-                    imageUrl: `http://localhost:1337${url}`,
+                    imageUrl: `https://vinuarts.com${url}`,
                   })
                 );
               }}
